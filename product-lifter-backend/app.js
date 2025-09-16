baf86406617835688312fc5ca7e8b5df46aa52a8
Ahmed Davila Guerrero <c-aguerrero@kahua.com>
2025-09-15 21:21:05 -0300

    useEffect()

--- product-lifter-backend/app.js
+++ product-lifter-backend/app.js
@@ -1,13 +1,20 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const productRoutes = require('./routes/product')
-const path = require('node:path')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const productRoutes = require("./routes/product");
+const path = require("node:path");
 
-const app = express()
+const corsOption = {
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use('/public', express.static(path.join(__dirname, '/storage/images')))
-app.use('/v1', productRoutes)
+const app = express();
 
-module.exports = app
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors(corsOption));
+app.use("/public", express.static(path.join(__dirname, "/storage/images")));
+app.use("/v1", productRoutes);
+
+module.exports = app;
--- product-lifter-frontend/src/Product/components/ProductLayout/ProductView/ProductList.js
+++ product-lifter-frontend/src/Product/components/ProductLayout/ProductView/ProductList.js
@@ -1,7 +1,38 @@
 import React, { useState, useEffect } from "react";
 import Loading from "./Loading";
+import axios from "axios";
+
+async function getProducts() {
+  try {
+    const response = await axios({
+      url: `http://localhost:8080/v1/getAllProducts`,
+      method: "GET",
+    });
+
+    return response;
+  } catch (e) {
+    console.log(e);
+  }
+}
 
 const ProductList = ({ load = true }) => {
+  const [products, setProducts] = useState([]);
+  const [productCounter, setProductCounter] = useState(0);
+
+  useEffect(() => {
+    async function loadProducts() {
+      const response = await getProducts();
+      if (response.status === 200) {
+        setProducts(response.data);
+        setProductCounter(Object.keys(response.data).length);
+      }
+    }
+
+    loadProducts();
+  }, [productCounter]);
+
+  console.log(products);
+
   return load ? <Loading></Loading> : null;
 };
 
