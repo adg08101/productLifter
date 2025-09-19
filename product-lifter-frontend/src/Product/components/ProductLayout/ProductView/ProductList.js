ddff371593c30ec7c1c1d0d9f04608ef288096ca
Ahmed Davila Guerrero <c-aguerrero@kahua.com>
2025-09-19 12:49:40 -0300

    axios useEffect()

--- product-lifter-frontend/src/Product/components/ProductLayout/ProductView/ProductList.js
+++ product-lifter-frontend/src/Product/components/ProductLayout/ProductView/ProductList.js
@@ -12,19 +12,70 @@ const ProductList = () => {
   const [productCounter, setProductCounter] = useState(0);
   const [refresh, setRefresh] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(0);
+  const productPaginatorLimit = 4;
 
   useEffect(() => {
     async function loadProducts() {
       const response = await getProducts();
       if (response.status === 200) {
-        setProducts(response.data);
+        const auxArray = response.data;
+        const fragmentSize = productPaginatorLimit;
+        const splittedArray = [];
+
+        for (let i = 0; i < auxArray.length; i += fragmentSize) {
+          splittedArray.push(auxArray.slice(i, i + fragmentSize));
+        }
+
+        setProducts(splittedArray);
         setProductCounter(Object.keys(response.data).length);
         setIsLoading(false);
       }
     }
 
     loadProducts();
-  }, [productCounter, refresh]);
+  }, [refresh]);
+
+  const footer = function footer() {
+    return (
+      <>
+        <ManualFetch
+          onClickFunc={() => {
+            setRefresh(!refresh);
+          }}
+          messageOne="refresh_now"
+          messageTwo="refresh_now"
+        ></ManualFetch>
+        <AddButton
+          text="Add button"
+          onClickFunc={() => {
+            setCurrentPage(1);
+          }}
+        />
+      </>
+    );
+  };
+
+  const paginator = function paginator() {
+    return (
+      <>
+        <Section>
+          <Paginator current={1} total={productCounter}></Paginator>
+        </Section>
+      </>
+    );
+  };
+
+  function getProductsByIndex() {
+    const productsChunk = products[currentPage];
+    const productsChunkArray = [];
+    productsChunk.forEach((p) => {
+      productsChunkArray.push(p);
+    });
+    productsChunkArray.map((p) => {
+      return <Card props={p}></Card>;
+    });
+  }
 
   if (isLoading) {
     return <Loading></Loading>;
@@ -34,41 +85,18 @@ const ProductList = () => {
     return (
       <>
         <Box>No products are found try adding new ones</Box>
-
-        <ManualFetch
-          onClickFunc={() => {
-            setRefresh(!refresh);
-          }}
-          messageOne="refresh_now"
-          messageTwo="refresh_now"
-        ></ManualFetch>
-        <AddButton text="Add button" />
+        {footer()}
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Columns>{getProductsByIndex}</Columns>
+        {footer()}
+        {paginator()}
       </>
     );
   }
-
-  return (
-    <>
-      <Columns>
-        <Card props={products[0]}></Card>
-        <Card props={products[0]}></Card>
-        <Card props={products[0]}></Card>
-        <Card props={products[0]}></Card>
-      </Columns>
-
-      <ManualFetch
-        onClickFunc={() => {
-          setRefresh(!refresh);
-        }}
-        messageOne="refresh_now"
-        messageTwo="refresh_now"
-      ></ManualFetch>
-      <AddButton text="Add button" />
-      <Section>
-        <Paginator current={1} total={productCounter}></Paginator>
-      </Section>
-    </>
-  );
 };
 
 export default ProductList;
